873f47928eb9ae9f5c79bcc9842fe0919b068d47
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-11-03 16:23:02 +0800

    cleanup

--- home.nix
+++ home.nix
@@ -17,8 +17,8 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    pkgs.nodejs_22
+  home.packages = with pkgs; [
+    nodejs_22
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
